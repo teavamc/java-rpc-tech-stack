7b2b8c01c4ddbaccbb4e038e760a0efe2f3b2f1c
teavamc <teavamc@163.com>
2021-01-21 14:50:29 +0800

    feat:一些补充

---
+++ rpc-service-two/start/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+
+-- 插入数据，分别是 姓名（id）、年龄、身高体、重
+INSERT INTO `users` VALUES ('1', 'Mac', '123');
+INSERT INTO `users` VALUES ('2', 'Jack', '432');
+
